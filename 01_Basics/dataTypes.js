266edeec06abdf8090560e270c2a67532eebbcdb
saurabhpal102 <155946347+saurabhpal102@users.noreply.github.com>
2024-01-08 09:24:39 +0000

    dataType2

---
+++ 01_Basics/dataTypes.js
@@ -0,0 +1,12 @@
+const candidateName = "Saurabh"; //const value need to define at the time of declration and value of const can`t be changed
+let city = "Kanpur"; // to declare any value this datatype is recomended
+var nearPlace = "Barra 2"; //not recomended due to scope issue
+state = "UttarPradesh";// declration is correct but not recomended
+var pincode; //undefined right now but we can assign a varriable after
+
+// candidateName = "Anant";
+city = "PrayagRaj";
+nearPlace = "Kasari masari";
+state = "uttrakhand";
+pincode = 208027;
+console.table([candidateName,city,nearPlace,state,pincode]);
---
+++ 01_Basics/dataTypes02.js
@@ -0,0 +1,14 @@
+// data type in JS
+
+// 1. Primitive
+//number => 2 to power 53
+//bigint
+//String ""
+// bollean true/false
+// null standalone value
+// undefined
+// symbol unique
+
+// 2. non primitive
+console.log(typeof undefined);  //undefined
+console.log(typeof null);       //object
