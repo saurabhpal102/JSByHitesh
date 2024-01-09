01f9f95a0accbc1732398e7aedf8d887c741dced
saurabhpal102 <155946347+saurabhpal102@users.noreply.github.com>
2024-01-09 04:34:46 +0000

    05_comparision

---
+++ 01_Basics/05_comparision.js
@@ -0,0 +1,11 @@
+console.log("02" > 1);
+console.log("2" >= "02");
+console.log(null > 0);
+console.log(null < 0);
+console.log(null <= 0);
+console.log(null >= 0);
+console.log(null == 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined <= 0);
+console.log("2" === 2);
